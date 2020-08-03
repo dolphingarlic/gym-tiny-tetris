c4bf5ca897baeb86011ea2de094aac27112be83f
dolphingarlic <dolphingarlic@gmail.com>
2020-08-03 22:26:26 +0200

    Installed importlib-resources

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name='gym_tiny_tetris',
     version='0.1',
-    install_requires=['gym', 'numpy']
+    install_requires=['gym', 'numpy', 'importlib-resources']
 )
